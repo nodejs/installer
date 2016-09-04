bed97408865935ec38c8ced993bbe550001928b1
delvedor <tommydelved@gmail.com>
2016-09-04 15:43:57 +0200

    Updated window size, setted resizable to false

--- main.js
+++ main.js
@@ -10,7 +10,11 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    resizable: false
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
